eb692e2a4f01c99abe7eee4aeeeb44d2c8be5965
lostways <andrew@lostways.com>
2023-12-26 14:42:04 -0800

    updating grayscale palette

--- src/pil2ansi.py
+++ src/pil2ansi.py
@@ -1,8 +1,6 @@
 import shutil
-import math
 from dataclasses import dataclass
-from typing import Protocol, Tuple, Literal, LiteralString
-import numpy as np
+from typing import Protocol, Tuple, Literal, LiteralString, Union
 from PIL import Image
 
 # Get terminal width/height
@@ -12,23 +10,23 @@ TERMINAL_HEIGHT = shutil.get_terminal_size().lines
 # PIL color modes
 PIL_COLOR = Literal["RGBA", "LA"]
 PIXEL_RGBA = Tuple[int, int, int, int]
+PIXEL_LA = Tuple[int, int]
 
 
 # Palettes for converting pixels to characters
 class Palette(Protocol):
-    def pixel_to_char(self, pixel_fg: PIXEL_RGBA, pixel_bg: PIXEL_RGBA) -> str:
+    def pixel_to_color(self, pixel_fg: Tuple, pixel_bg: Tuple) -> str:
         ...
 
     @property
     def pil_color(self) -> PIL_COLOR:
         ...
 
-
 @dataclass
 class PaletteColor:
     pil_color: PIL_COLOR = "RGBA"
 
-    def pixel_to_char(self, pixel_fg: PIXEL_RGBA, pixel_bg: PIXEL_RGBA) -> str:
+    def pixel_to_color(self, pixel_fg: PIXEL_RGBA, pixel_bg: PIXEL_RGBA) -> str:
         r, g, b, a = pixel_fg
         r2, g2, b2, a2 = pixel_bg
 
@@ -43,17 +41,23 @@ class PaletteGrayscale:
     invert: bool = False
     pil_color: PIL_COLOR = "LA"
 
-    def pixel_to_char(self, pixel: Tuple[int, int]) -> str:
-        p, _ = pixel
+    def pixel_to_color(self, pixel_fg: PIXEL_LA, pixel_bg: PIXEL_LA) -> str:
+        p1, a1 = pixel_fg
+        p2, a2 = pixel_bg
+
+        format1 = 1 if a1 == 0 else 5  # 5 is 8-bit, 1 is transparent
+        format2 = 1 if a2 == 0 else 5  # 5 is 8-bit, 1 is transparent
 
         num_values = 23
 
         if self.invert == True:
-            val = 255 - int(p * num_values / 255)
+            val_fg = 255 - int(p1 * num_values / 255)
+            val_bg = 255 - int(p2 * num_values / 255)
         else:
-            val = 232 + int(p * num_values / 255)
+            val_fg = 232 + int(p1 * num_values / 255)
+            val_bg = 232 + int(p2 * num_values / 255)
 
-        return f"\033[0;48;5;{val}m \033[0m"
+        return f"\033[38;{format1};{val_fg};48;{format2};{val_bg}m"
 
 
 @dataclass
@@ -116,11 +120,12 @@ def convert_img(
     for i in range(len(pixels) // img.width):
         for j in range(img.width):
             if i % 2 == 0:
-                pixel_fg = pixels[i * img.width + j]
+
+                pixel_fg: Tuple = pixels[i * img.width + j]
                 if i < img.height - 1:
-                    pixel_bg= pixels[(i + 1) * img.width + j] 
+                    pixel_bg: Tuple = pixels[(i + 1) * img.width + j] 
                 else: 
-                    pixel_bg = (255,255,255,0)
+                    pixel_bg: Tuple = (255,255,255,0) if palette.pil_color == "RGBA" else (255,0)
 
                 if alpha == False:
                     pixel_fg = pixel_fg[:-1] + (255,)
@@ -129,9 +134,9 @@ def convert_img(
                 if pixel_fg[-1] == 0 and pixel_bg[-1] == 0:
                     ascii_str += f"{reset_char}{transparent_char}"
                 elif pixel_fg[-1] == 0:
-                    ascii_str += f"{reset_char}{palette.pixel_to_char(pixel_fg=pixel_bg, pixel_bg=pixel_bg)}{unicode_lower_char}"
+                    ascii_str += f"{reset_char}{palette.pixel_to_color(pixel_fg=pixel_bg, pixel_bg=pixel_bg)}{unicode_lower_char}"
                 else:
-                    ascii_str += f"{reset_char}{palette.pixel_to_char(pixel_fg=pixel_fg, pixel_bg=pixel_bg)}{unicode_upper_char}"
+                    ascii_str += f"{reset_char}{palette.pixel_to_color(pixel_fg=pixel_fg, pixel_bg=pixel_bg)}{unicode_upper_char}"
             else:
                 continue
 
